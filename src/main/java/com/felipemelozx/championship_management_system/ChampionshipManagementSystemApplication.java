c5836c2e469127f57fe08c35ce21372cb786ed8f
Felipe <felipemelozx@gmail.com>
2024-07-26 16:57:29 -0300

    🎉 Commit inicial

---
+++ src/main/java/com/felipemelozx/championship_management_system/ChampionshipManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.felipemelozx.championship_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChampionshipManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChampionshipManagementSystemApplication.class, args);
+	}
+
+}
